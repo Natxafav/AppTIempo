7cd25c680335a76ba4426a75f88ae5426c2578bb
Natxafav <Natidevg@gmail.com>
2023-10-01 20:55:18 +0100

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -5,5 +5,6 @@ y añade tu apikey tal como se muestra a continuación
 const apikey = "a99e15d23a2e9r7t38gcgc67y2s64676";
 //este es el formato de apikey que obtendrás de la web
 //Lo exportas desde aquí para darle acceso al index.js desde
-//el cual lo estarás llamando para que funcione la app.
+//el cual lo estarás llamando para que funcione la app o directamente lo insertas en 
+//tu js y accedes directamente.
 export default apikey;
